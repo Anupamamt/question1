a7f55a5256d3be8afb8452f1a259c5113b9e5415
Anupama M T <anupamamadhu121@gmail.com>
2022-09-25 14:53:55 +0530

    Add project files.

---
+++ practise1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practise1
+{
+    class Parent
+    {
+        public string Family_name="fname";
+    }
+    class Child : Parent
+    {
+        public string Name;
+        public string Occupation;
+        public int Age;
+        public void childDetails(string n,string o,int a)
+        {
+            Name=n;
+            Occupation=o;
+            Age=a;
+        }
+        public void display()
+        {
+            Console.WriteLine("Name : " + Name + " Occupation : " + Occupation + " Age : " + Age +" Family name : "+Family_name);
+        }
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Child child = new Child();
+            child.childDetails("anu", "engineer", 22);
+            child.display();
+            Console.ReadKey();
+        }
+    }
+}
